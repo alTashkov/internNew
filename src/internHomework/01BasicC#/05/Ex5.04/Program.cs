84079f41809a640beef8e4757adbd9d6e5ceb253
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-07-25 13:44:46 +0300

    Add ex. 1-6 of hw 5

---
+++ src/internHomework/01BasicC#/05/Ex5.04/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter first number: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double b = double.Parse(Console.ReadLine());
+        
+        Console.Write("Enter third number: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double[] d = { a, b, c };
+        
+        string sign = "";
+
+        int minusCount = 0;
+        
+        for (int i = 0; i < d.Length; i++)
+        {
+            if (d[i] < 0)
+            {
+                minusCount++;
+            }
+            else if (d[i] == 0)
+            {
+                sign = "0";
+                break;
+            }
+        }
+
+        if (minusCount % 2 == 0 && sign != "0")
+        {
+            sign = "+";
+        }
+        else if(minusCount % 2 != 0 && sign != "0")
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Multiplication sign of {a},{b},{c}: {sign}.");
+        
+    }
+}
---
+++ src/internHomework/01BasicC#/05/Ex5.05/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter first number: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double biggest = a;
+
+        biggest = biggest > b ? biggest : b > biggest ? b : 0;
+        biggest = biggest > c ? biggest : c > biggest ? c : 0;
+
+        Console.WriteLine($"Biggest number of {a},{b},{c} is: {biggest}");
+
+    }
+}
