893ef9f6632c18d71ef7395675d7fc4e692bd11b
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-07-29 13:56:01 +0300

    Fix issues found from feedback.

--- src/internHomework/01BasicC#/05/Ex5.07/Program.cs
+++ src/internHomework/01BasicC#/05/Ex5.07/Program.cs
@@ -11,39 +11,54 @@
         Console.Write("Enter num 3: ");
         int n3 = int.Parse(Console.ReadLine()!);
 
-        int largest = n1;
+        int largest = 0;
         int middle = 0;
         int smallest = 0;
 
-        if (largest < n2)
+        if (n1 > n2)
+        {
+            largest = n1;
+            if (n3 > n1)
+            {
+                largest = n3;
+                middle = n1;
+                smallest = n2;
+            }
+            if (n2 <= n3 && largest == n1)
+            {
+                middle = n3;
+                smallest = n2;
+            }
+            if (n2 >= n3 && largest == n1)
+            {
+                middle = n2;
+                smallest = n3;
+            }
+            
+        }
+        else if (n1 < n2)
         {
             largest = n2;
-            middle = n1;
-            smallest = n3;
-
-            if(middle < smallest)
+            if (n3 > n2)
             {
-                int temp = smallest;
-                smallest = middle;
-                middle = temp;
+                largest = n3;
+                middle = n2;
+                smallest = n1;
+            }
+            if (n1 <= n3 && largest == n2)
+            {
+                middle = n3;
+                smallest = n1;
+            }
+            if (n1 >= n3 && largest == n2)
+            {
+                middle = n1;
+                smallest = n3;
             }
         }
 
-        if (largest < n3)
-        {
-            largest = n3;
-            middle = n1;
-            smallest = n2;
 
-            if (middle < smallest)
-            {
-                int temp = smallest;
-                smallest = middle;
-                middle = temp;
-            }
-        }
-        
-        Console.Write($"Numbers descending: {largest},{middle},{smallest}");
+            Console.Write($"Numbers descending: {largest},{middle},{smallest}");
 
     }
 }
