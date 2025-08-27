6a827f58b0d8b930632e86331578349abd7692fb
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-08-27 11:34:32 +0300

    Remove unused namespaces.

--- src/internHomework/03PrinciplesOOP/04/4.02/GenericList.cs
+++ src/internHomework/03PrinciplesOOP/04/4.02/GenericList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
+﻿using System.Text;
 namespace _4._02
 {
     class GenericList<T> : List<T>
--- src/internHomework/03PrinciplesOOP/04/4.02/PathStorage.cs
+++ src/internHomework/03PrinciplesOOP/04/4.02/PathStorage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _4._02
+﻿namespace _4._02
 {
     class PathStorage
     {
